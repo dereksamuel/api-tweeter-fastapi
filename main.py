b27cb33f346e2398502bfe21ede3073e15d5348c
Derek Paul <dereksamuelgr@gmail.com>
2021-11-03 11:41:36 -0500

    add tweet_post

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Tweet(BaseModel):
     max_length=256
   );
   created_at: datetime = Field(default=datetime.utcnow());
-  update_at: Optional[datetime] = Field(default=None);
+  updated_at: Optional[datetime] = Field(default=None);
   by: User = Field(...);
 
 # Path operations
@@ -186,11 +186,39 @@ def home():
   response_model=Tweet,
   status_code=status.HTTP_201_CREATED,
   summary="Post ONE Tweet",
-  description="Post ONE tweet",
   tags=["Tweets"]
 )
-def post():
-  pass;
+def post(tweet: Tweet = Body(...)):
+  """
+    # Post a tweet
+    This path operation REGISTER a user in the app.
+
+    ## Parameters:
+      - Request body parameter
+        - **tweet**: Tweet
+
+    Returns a json with the basic user information
+      - tweet_id: UUID
+      - message: str
+      - created_at: datetime
+      - updated_at: Optional[datetime]
+      - by: User
+  """
+  
+  with open("tweets.json", "r+", encoding="utf-8") as file: # leer y escribir r+
+    tweets = json.loads(file.read());
+
+    tweet_dict = tweet.dict(); # lo pone fastapi para transformar json a dict PERO ATRIBUTOS MANUALMENTE
+    tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"]);
+    tweet_dict["created_at"] = str(tweet_dict["created_at"]);
+    tweet_dict["updated_at"] = str(tweet_dict["updated_at"]);
+    tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"]);
+    tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"]);
+
+    tweets.append(tweet_dict);
+    file.seek(0); # moverme al primer byte del archivo (ESCRIBIR DESDE CERO en el file)
+    file.write(json.dumps(tweets)); # Convierto a json el dict
+    return tweet;
 
 ### Get One tweet
 @app.get(
